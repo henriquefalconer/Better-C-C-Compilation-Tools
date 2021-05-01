92eae488c1589129450337824060b139de637492
Henrique <henrique@falconer.com.br>
2021-05-01 12:09:39 -0300

    feat: separate install from better_c_cpp

---
+++ better_c_cpp.sh
@@ -0,0 +1,64 @@
+# ------ Start of Better C/C++ Compilation Tools ------
+
+echoeval() {
+	PURPLE='\e[0;35m'
+	NOCOLOR='\e[0m'
+	echo "${PURPLE}$ $1${NOCOLOR}"
+	eval $1
+}
+
+out() {
+	echoeval ./.a.out
+}
+
+crun() {
+	echoeval "gcc -ansi -pedantic -Wall -fexceptions -g -o .a.out $1" && out
+}
+
+cnew() {
+	printf '#include <stdio.h>\n\nint onePlusOne() { return 1 + 1; }\n\nint main() {\n	int two = onePlusOne();\n\n	printf("1 + 1 = %%d%s", two);\n\n	return 0;\n}\n' '\n' >$1.c
+}
+
+cpprun() {
+	echoeval "g++ -std=c++11 -pedantic -Wall -fexceptions -g -o .a.out $1" && out
+}
+
+cppnew() {
+	echo '#include <iostream>\n#include <string>\n\nusing namespace std;\n\nstring exercicio() { return "Hello world!"; }\n\nint main() {\n	cout << exercicio();\n\n	return 0;\n}' >$1.cpp
+}
+
+hidevscc() {
+	[ -d .vscode ] || mkdir .vscode
+	echo '{\n	"files.exclude": {\n		".a.out.dSYM": true,\n		".a.out": true\n	}\n}' >.vscode/settings.json
+}
+
+chelp() {
+	GREEN='\e[32m'
+	LIGHTBLUE='\e[94m'
+	LIGHTGREY='\e[37m'
+	NOCOLOR='\e[0m'
+	LINEBREAK='\n\t\t\t\t⮑  '
+	if [ "$1" = "new" ]; then
+		echo '\n🎉 Foram criados os seguintes comandos!'
+	else
+		echo '\nComandos para rodar programas em C/C++! 💻'
+	fi
+	INSTRUCTIONS="
+${GREEN}cnew ${LIGHTBLUE}[nome do arquivo]${NOCOLOR}\t\tgera um novo arquivo C na pasta atual, com um template inicial.
+
+${GREEN}crun ${LIGHTBLUE}[nome do arquivo.c]${NOCOLOR}\tcompila e roda um código em C (use TAB para completar o nome${LINEBREAK}do arquivo ao escrever na linha de comando).
+
+${GREEN}cppnew ${LIGHTBLUE}[nome do arquivo]${NOCOLOR}\tgera um novo arquivo C++ na pasta atual, com um template inicial.
+
+${GREEN}cpprun ${LIGHTBLUE}[nome do arquivo.cpp]${NOCOLOR}\tcompila e roda um código em C++ (use TAB para completar o nome${LINEBREAK}do arquivo ao escrever na linha de comando).
+
+${GREEN}out${NOCOLOR}\t\t\t\troda o último código em C/C++ compilado com ${LIGHTBLUE}crun${NOCOLOR} ou ${LIGHTBLUE}cpprun${NOCOLOR} na${LINEBREAK}pasta atual.
+
+${GREEN}hidevscc${NOCOLOR}\t\t\tcaso esteja usando VS Code, este comando torna invisíveis os${LINEBREAK}arquivos de compilação para não poluir a área de trabalho.
+"
+	echo "\n${INSTRUCTIONS}\n"
+	[ "$1" = "new" ] && echo "Para visualizar este menu novamente, é só digitar ${LIGHTBLUE}chelp${NOCOLOR} 😉\n"
+	echo "Feito por ${LIGHTBLUE}@henriquefalconer${NOCOLOR} (https://github.com/henriquefalconer)\n"
+}
+
+# ------ End of Better C/C++ Compilation Tools ------
--- install.sh
+++ install.sh
@@ -1,61 +1,36 @@
 #!/bin/bash
+set -u
 
-echoeval() {
-	PURPLE='\e[0;35m'
-	NOCOLOR='\e[0m'
-	echo "${PURPLE}$ $1${NOCOLOR}"
-	eval $1
+abort() {
+	printf "%s\n" "$@"
+	exit 1
 }
 
-out() {
-	echoeval ./.a.out
-}
+if [ -z "${BASH_VERSION:-}" ]; then
+	abort "Bash é necessário para rodar este script."
+fi
 
-crun() {
-	echoeval "gcc -ansi -pedantic -Wall -fexceptions -g -o .a.out $1" && out
-}
+# Primeiro verifica o sistema operacional.
+OS="$(uname)"
+if [[ "$OS" == "Linux" ]]; then
+	RUNNING_LINUX=1
+elif [[ "$OS" != "Darwin" ]]; then
+	abort "Este script apenas suporta macOS e Linux."
+fi
 
-cnew() {
-	printf '#include <stdio.h>\n\nint onePlusOne() { return 1 + 1; }\n\nint main() {\n	int two = onePlusOne();\n\n	printf("1 + 1 = %%d%s", two);\n\n	return 0;\n}\n' '\n' > $1.c
-}
+# Realiza o curl para obter código.
+BETTERCCPP="$(curl -fsSL https://raw.githubusercontent.com/henriquefalconer/better-c-cpp-compilation-tools/main/install.sh)"
 
-cpprun() {
-	echoeval "g++ -std=c++11 -pedantic -Wall -fexceptions -g -o .a.out $1" && out
-}
+# Seleciona o path de instalação.
+if [[ "$SHELL" == "/bin/zsh" ]]; then
+	# On ARM macOS, this script installs to /opt/homebrew only
+	BETTERCCPP >> ~/.zshenv
+	source ~/.zshenv
+else
+	# On Intel macOS, this script installs to /usr/local only
+	BETTERCCPP >> ~/.bashrc
+	source ~/.bashrc
+fi
 
-cppnew() {
-	echo '#include <iostream>\n#include <string>\n\nusing namespace std;\n\nstring exercicio() { return "Hello world!"; }\n\nint main() {\n	cout << exercicio();\n\n	return 0;\n}' > $1.cpp
-}
-
-hidevscc() {
-	[ -d .vscode ] || mkdir .vscode; echo '{\n	"files.exclude": {\n		".a.out.dSYM": true,\n		".a.out": true\n	}\n}' > .vscode/settings.json
-}
-
-chelp() {
-	GREEN='\e[32m'
-	LIGHTBLUE='\e[94m'
-	LIGHTGREY='\e[37m'
-	NOCOLOR='\e[0m'
-	LINEBREAK='\n\t\t\t\t⮑  '
-	if [ "$1" = "new" ]; then
-		echo '\n🎉 Foram criados os seguintes comandos!'
-	else 
-		echo '\nComandos para rodar programas em C/C++! 💻'
-	fi
-   INSTRUCTIONS="
-${GREEN}cnew ${LIGHTBLUE}[nome do arquivo]${NOCOLOR}\t\tgera um novo arquivo C na pasta atual, com um template inicial.
-
-${GREEN}crun ${LIGHTBLUE}[nome do arquivo.c]${NOCOLOR}\tcompila e roda um código em C (use TAB para completar o nome${LINEBREAK}do arquivo ao escrever na linha de comando).
-
-${GREEN}cppnew ${LIGHTBLUE}[nome do arquivo]${NOCOLOR}\tgera um novo arquivo C++ na pasta atual, com um template inicial.
-
-${GREEN}cpprun ${LIGHTBLUE}[nome do arquivo.cpp]${NOCOLOR}\tcompila e roda um código em C++ (use TAB para completar o nome${LINEBREAK}do arquivo ao escrever na linha de comando).
-
-${GREEN}out${NOCOLOR}\t\t\t\troda o último código em C/C++ compilado com ${LIGHTBLUE}crun${NOCOLOR} ou ${LIGHTBLUE}cpprun${NOCOLOR} na${LINEBREAK}pasta atual.
-
-${GREEN}hidevscc${NOCOLOR}\t\t\tcaso esteja usando VS Code, este comando torna invisíveis os${LINEBREAK}arquivos de compilação para não poluir a área de trabalho.
-"
-	echo "\n${INSTRUCTIONS}\n"
-	[ "$1" = "new" ] && echo "Para visualizar este menu novamente, é só digitar ${LIGHTBLUE}chelp${NOCOLOR} 😉\n"
-	echo "Feito por ${LIGHTBLUE}@henriquefalconer${NOCOLOR} (https://github.com/henriquefalconer)\n"
-}
+# Diz oi:
+chelp new

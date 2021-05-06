fd37fee6fc008203b7afc67931193514f4044c24
Henrique <henrique@falconer.com.br>
2021-05-06 10:28:33 -0300

    feat: update checks & description

--- funcs.sh
+++ funcs.sh
@@ -1,7 +1,7 @@
 
 # ------ Start of Better C/C++ Tools ------
 
-BETTERCCPPVERS='1.0.0'
+BETTERCCPPVERS='X.X.X'
 
 # Formatação de strings
 LIGHTBLUE='\e[94m'
@@ -19,6 +19,36 @@ if [[ $OS == 'Linux' ]]; then
     LINEBREAK=''
 fi
 
+# A partir das releases do projeto, obtém o valor que corresponde com a chave passada da última versão.
+getlatestversiondata() {
+    printf "$(awk "/^    \"$1\": .+/{print}" <(printf "%s\n" "$CCPPRELEASES") | head -1 | sed -e "s/^    \"$1\": \"\{0,1\}//g" -e "s/\"\{0,1\},\{0,1\}$//g")"
+}
+
+# Realiza o curl para obter as releases, caso esteja instalando pela primeira vez.
+crefreshversions() {
+    SECONDSSINCELASTRUN=$(($(date +%s) - LASTTIMECREFRESHRUN))
+    if [[ "$SECONDSSINCELASTRUN" -gt 3600 || $1 == 'force' ]]; then
+        LASTTIMECREFRESHRUN=$(date +%s)
+        CCPPRELEASES=$(curl -s 'https://api.github.com/repos/henriquefalconer/better-c-cpp-tools/releases')
+        if [[ $CCPPRELEASES =~ .*"API rate limit exceeded".* ]]; then
+            printf "\nA API do GitHub restringiu seu acesso às versões do projeto. Adicione um tópico em ${TTYBOLD}https://github.com/henriquefalconer/better-c-cpp-tools/issues${TTYRESET} caso isso ocorra repetidamente.\n\n"
+            CREFRESHFAILED=true
+        else
+            LATESTVERSIONNAME=$(getlatestversiondata name)
+            CREFRESHFAILED=false
+        fi
+    fi
+}
+
+ccheckupdate() {
+    crefreshversions
+    if [ $CREFRESHFAILED = false ]; then
+        if [ "$LATESTVERSIONNAME" != "$BETTERCCPPVERS" ]; then
+            printf "\n${TTYBOLD}Better C/C++ Tools v${LATESTVERSIONNAME}$TTYRESET já está disponível! Rode ${LIGHTBLUE}cupdate$1$NOCOLOR para visualizar as novas funcionalidades 🚀\n\n"
+        fi
+    fi
+}
+
 printfeval() {
     printf "$PURPLE$ $1$NOCOLOR\n"
     eval $1
@@ -28,6 +58,7 @@ alias out='printfeval ./.a.out'
 
 crun() {
     printfeval "gcc -ansi -pedantic -Wall -fexceptions -g -o .a.out $1" && out
+    ccheckupdate
 }
 
 cnew() {
@@ -41,6 +72,7 @@ ctempl() {
 
 cpprun() {
     printfeval "g++ -std=c++11 -pedantic -Wall -fexceptions -g -o .a.out $1" && out
+    ccheckupdate
 }
 
 cppnew() {
@@ -66,14 +98,29 @@ hidevscc() {
 }
 
 cupdate() {
-    printf "\n🔎  Baixando mais nova versão das funções e templates..."
-    # TODO: implementar verificação da atualização, utilizando a variável BETTERCCPPVERS
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/henriquefalconer/better-c-cpp-tools/main/install.sh)" >/dev/null 2>&1
-    # Verifica se os comandos foram carregados com o "source".
-    if ! command -v chelp &>/dev/null; then
-        printf " ${RED}Erro${NOCOLOR}\n\n"
-    else
+    crefreshversions force
+
+    if [ $CREFRESHFAILED = true ]; then
+        return 1
+    fi
+    
+    if [ "$LATESTVERSIONNAME" = "$BETTERCCPPVERS" ]; then
+        printf "\nVocê já possui a versão mais recente do Better C/C++ Tools.\n\n"
+        return 1
+    fi
+
+    LATESTVERSIONDESC=$(getlatestversiondata body | sed -e "s/\`/\` /g" -e "s/ \` / \\$LIGHTBLUE/g" -e "s/\` /\\$NOCOLOR/g")
+    printf "\nNovidades do Better C/C++ Tools v$LATESTVERSIONNAME 🚀\n\n$LATESTVERSIONDESC\n\nVocê gostaria de baixar esta versão? (Y/n) "
+    read USERRESPONSE
+
+    if [[ "$USERRESPONSE" == 'Y' || "$USERRESPONSE" == 'y' || "$USERRESPONSE" == '' ]]; then
+        printf "\n🔎  Baixando mais nova versão das funções e templates..."
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/henriquefalconer/better-c-cpp-tools/main/install.sh)" >/dev/null 2>&1
         printf " Feito!\n\n"
+        # Verifica se os comandos foram carregados com o "source".
+        if [ "$LATESTVERSIONNAME" != "$BETTERCCPPVERS" ]; then
+            printf "\n\nPara utilizar a nova versão, feche este shell e abra-o novamente.\n\n"
+        fi
     fi
 }
 
@@ -112,7 +159,8 @@ chelp() {
     printcommand 'hidevscc' '' 'caso esteja usando VS Code, este comando torna invisíveis os arquivos de compilação para não poluir a área de trabalho.'
     printcommand 'cupdate' '' "baixa e atualiza o \\${TTYBOLD}Better C/C++ Tools\\$TTYRESET para a última versão disponível."
     printf "${TTYBOLD}Better C/C++ Tools v${BETTERCCPPVERS}$TTYRESET - feito por $LIGHTBLUE@henriquefalconer$NOCOLOR (https://github.com/henriquefalconer)\n\n"
-    printf "Sugestões ou problemas podem ser submetidos aqui: ${TTYBOLD}https://github.com/henriquefalconer/better-c-cpp-tools/issues$TTYRESET\n\n"
+    printf "Sugestões ou problemas podem ser submetidos aqui: ${TTYBOLD}https://github.com/henriquefalconer/better-c-cpp-tools/issues$TTYRESET\n"
+    ccheckupdate
 }
 
 # ------ End of Better C/C++ Tools ------
--- install.sh
+++ install.sh
@@ -23,6 +23,20 @@ NOCOLOR='\e[0m'
 TTYBOLD="\033[1;39m"
 TTYRESET="\033[1;0m"
 
+# A partir das releases do projeto, obtém o valor que corresponde com a chave passada da última versão.
+getlatestversiondata() {
+    printf "$(awk "/^    \"$1\": .+/{print}" <(printf "%s\n" "$CCPPRELEASES") | head -1 | sed -e "s/^    \"$1\": \"\{0,1\}//g" -e "s/\"\{0,1\},\{0,1\}$//g")"
+}
+
+# Realiza o curl para obter as releases, caso esteja instalando pela primeira vez.
+if [ -z "$LATESTVERSIONNAME" ]; then
+    CCPPRELEASES=$(curl -s 'https://api.github.com/repos/henriquefalconer/better-c-cpp-tools/releases')
+    if [[ $CCPPRELEASES =~ .*"API rate limit exceeded".* ]]; then
+        abort "A API do GitHub restringiu seu acesso às versões do projeto. Adicione um tópico em ${TTYBOLD}https://github.com/henriquefalconer/better-c-cpp-tools/issues${TTYRESET} e tente novamente mais tarde."
+    fi
+    LATESTVERSIONNAME=$(getlatestversiondata name)
+fi
+
 # Realiza o curl para obter os templates.
 printf "1/3 📄 Baixando templates de C/C++..."
 curl -fsSL https://raw.githubusercontent.com/henriquefalconer/better-c-cpp-tools/main/templates/template.c >~/.template.c
@@ -42,6 +56,7 @@ savefuncs() {
     curl -fsSL https://raw.githubusercontent.com/henriquefalconer/better-c-cpp-tools/main/funcs.sh >>$1
     printf " Feito!\n\n"
     printf "3/3 📀 Salvando-os em ${LIGHTBLUE}$1${NOCOLOR}..."
+    sed -i -e "s/BETTERCCPPVERS='X.X.X'/BETTERCCPPVERS='$LATESTVERSIONNAME'/g" $1
     source $1
     printf " Salvos!\n\n"
 }
